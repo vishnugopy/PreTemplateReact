e6d0de619a2e0f953883eb9cba264d500fc26bd9
vishnugopy <vishnugoby0010@gmail.com>
2021-10-15 20:56:16 +0200

    v1

--- src/components/Headsection/index.js
+++ src/components/Headsection/index.js
@@ -6,6 +6,10 @@ function HeadSection() {
     <section className="mainsection">
       <h1>The best place to fill your stomach.</h1>
       <h2>Eat-Drink-Visit-Enjoy</h2>
+      <div className="buttons">
+        <a href="" className="mainbutton">Visit Us</a>
+        <a href="tel:076" className="subbutton">Call Us</a>
+      </div>
     </section>
   );
 }
