e2aaa7a210c4acb33e0548c2552579a45a161ac6
vishnugopy <vishnugoby0010@gmail.com>
2021-10-18 23:05:28 +0200

    v1

--- src/components/Headsection/index.js
+++ src/components/Headsection/index.js
@@ -1,16 +1,24 @@
 import "./style.scss";
 
-
 function HeadSection() {
   return (
     <section className="mainsection">
-      <div className="mainleft">
-      <h1>The best place to fill your stomach.</h1>
-      <h2>Eat-Drink-Visit-Enjoy</h2>
+      <div className="first">
+        <div className="mainleft">
+          <h1>The best place to fill your stomach.</h1>
+          <h2>Eat-Drink-Visit-Enjoy</h2>
+        </div>
+        <div className="buttons">
+          <a href="" className="mainbutton">
+            Visit Us
+          </a>
+          <a href="tel:076" className="subbutton">
+            Call Us
+          </a>
+        </div>
       </div>
-      <div className="buttons">
-        <a href="" className="mainbutton">Visit Us</a>
-        <a href="tel:076" className="subbutton">Call Us</a>
+      <div className="seconde">
+        <div className="photobox"></div>
       </div>
     </section>
   );
