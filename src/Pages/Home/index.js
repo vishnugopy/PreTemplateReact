637f6529b778318cf496ce835b5d9efc36be575f
vishnugopy <vishnugoby0010@gmail.com>
2021-11-07 19:22:40 +0100

    v2 with uber and delivroo with resposive

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -3,6 +3,15 @@ import HomeSection from "../../components/Headsection";
 import Takeorder from "../../components/Takeorder";
 
 function Home() {
+ const bgcolorlist = ["#f9f6e7", "#e7f9e7" , "#f9eae7" , "#e7f9f9" , "#e9e7f9" , "#f7e7f9" , "#f9e7e8"];
+
+ window.onload = function(){
+  var arr = document.querySelector("body");
+  for(var i=0;i<bgcolorlist.length;i++){
+       arr.style.background = bgcolorlist[Math.floor(Math.random()*bgcolorlist.length)]
+  }
+}
+
   return (
     <section>
       <HomeSection />
